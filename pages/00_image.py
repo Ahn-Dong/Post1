55ad077a74517dcaec0b4f0a384e2fddcbf8edd3
Ahn-Dong <58833078+Ahn-Dong@users.noreply.github.com>
2025-06-21 13:10:08 +0900

    Create 00_image.py

---
+++ pages/00_image.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.image("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTE1dTFxNjV1Zm4xeW1tYTNwb2NpanBxeG02cDJzcDlsamQ1NmFhbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/BPJmthQ3YRwD6QqcVD/giphy.gif")
